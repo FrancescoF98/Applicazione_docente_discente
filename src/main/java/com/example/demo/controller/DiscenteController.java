9770fa5c5dda10beb01b4480e82452b20ff3585d
FrancescoF98 <f.fiocca@elitesoftwarehouse.com>
2025-07-07 14:20:35 +0200

    Aggiunto @crossorigin in controller

--- src/main/java/com/example/demo/controller/DiscenteController.java
+++ src/main/java/com/example/demo/controller/DiscenteController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/discenti")
 public class DiscenteController {
